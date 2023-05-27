f822ac05ba9f58fd8619a9d89b1f90b5eafaddca
Lara <lara@laranewsom.com>
2023-05-27 12:48:23 -0500

    added ContactComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule
