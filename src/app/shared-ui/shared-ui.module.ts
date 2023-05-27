d26e69b40ffbeb7f1fd28de7deac08739872920b
Lara <lara@laranewsom.com>
2023-05-27 13:05:59 -0500

    added SubHeroLogoComponent

--- src/app/shared-ui/shared-ui.module.ts
+++ src/app/shared-ui/shared-ui.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { SubHeroLogoComponent } from './sub-hero-logo/sub-hero-logo.component';
 
 
 
 @NgModule({
   declarations: [
-    BreadcrumbsComponent
+    BreadcrumbsComponent,
+    SubHeroLogoComponent
   ],
   imports: [
     CommonModule
