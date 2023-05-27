329895d69df8f75314d0c1feb37d396873cd89cf
Lara <lara@laranewsom.com>
2023-05-27 13:05:20 -0500

    added BreadcrumbsComponent

--- src/app/shared-ui/shared-ui.module.ts
+++ src/app/shared-ui/shared-ui.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BreadcrumbsComponent
+  ],
   imports: [
     CommonModule
   ]
