67b7aaa5a98cd1aee4cdbec6378bbb24de096d76
Lara <lara@laranewsom.com>
2023-05-29 23:50:14 -0500

    cleaned up a few things

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,14 +6,11 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  @Output() myEvent = new EventEmitter<string>();
-
   menuItemOne = 'Menu Item One';
   logoUrl = '../../assets/images/justlikepeople.png';
 
   clickMenuItem(name: string){
     console.log(name, ' was clicked!');
-    this.myEvent.emit(name);
   }
 }
 
